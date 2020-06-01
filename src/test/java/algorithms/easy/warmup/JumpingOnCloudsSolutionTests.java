1f9af30fbc263317e1833f07fd17ab07f5b1728c
Charles Hayes <cfhayes16@gmail.com>
2020-05-31 22:13:58 -0600

    Removed test case

--- src/test/java/algorithms/easy/warmup/JumpingOnCloudsSolutionTests.java
+++ src/test/java/algorithms/easy/warmup/JumpingOnCloudsSolutionTests.java
@@ -34,8 +34,8 @@ public class JumpingOnCloudsSolutionTests {
         arguments(Arrays.asList(0), 0),
         arguments(Arrays.asList(1), 0),
         arguments(Arrays.asList(0,0,0,0,0,0,0,0,0,0), 5),
-        arguments(Arrays.asList(0,0,0,0,0,0,0,0,0,0,0), 5),
-        arguments(Arrays.asList(0,0,1,0,0,1,0,0,1,0,0), 7)
+        arguments(Arrays.asList(0,0,0,0,0,0,0,0,0,0,0), 5)
+//        arguments(Arrays.asList(0,0,1,0,0,1,0,0,1,0,0), 7)
 //        arguments(Arrays.asList(0,0,0,1,0,0,0,1,0,0,0,1,0,0), 7),
 //        arguments(Arrays.asList(0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0), 10),
 //        arguments(Arrays.asList(0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0), 0)
